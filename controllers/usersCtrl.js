c6d390f65080cdaa3255a84f0e8c6c169a8ec3ea
Jeff Li <jeff.li@autocase.com>
2020-01-29 14:31:00 -0500

    added unit tests using mocha chai and sinon

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -3,13 +3,13 @@ const logger = require('../logger')
 const controller = {
 
     getAllUsers: (req, res) => {
-        res.status(200).send([])
+        res.status(200).json([])
     },
 
     getUserbyId: (req, res) => {
         const userId = req.params.userId
         logger.info(userId)
-        res.status(200).send({})
+        res.status(200).json({})
     }
 }
 
