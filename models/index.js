c337ade11e3622bfc9bba5088980c9e794434a9b
Jeff Li <jeff.li@autocase.com>
2021-12-19 16:28:40 -0500

    sequelize migration and seed

--- models/index.js
+++ models/index.js
@@ -5,9 +5,20 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 
 const sequelize = new Sequelize(config.url, config);
+// Test connection
+console.info("SETUP - Connecting database...")
+
+sequelize
+  .authenticate()
+  .then(() => {
+      console.info("INFO - Database connected.")
+  })
+  .catch((err) => {
+      console.error("ERROR - Unable to connect to the database:", err)
+  })
 
 const db = {};
 
